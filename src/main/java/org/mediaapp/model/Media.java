c7704f5aa459b4ea81918fcb2c308ba667563d34
Atti3284 <Nagyok3248994@gmail.com>
2025-05-27 19:41:04 +0200

    p

--- src/main/java/org/mediaapp/model/Media.java
+++ src/main/java/org/mediaapp/model/Media.java
@@ -1,7 +1,11 @@
 package org.mediaapp.model;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
