31e964db2e1f0bea3254fe5299bfd93b7378941a
Atti3284 <Nagyok3248994@gmail.com>
2025-05-27 14:25:10 +0200

    .

--- src/main/java/org/mediaapp/controller/RatingController.java
+++ src/main/java/org/mediaapp/controller/RatingController.java
@@ -2,7 +2,14 @@ package org.mediaapp.controller;
 
 import org.mediaapp.model.Rating;
 import org.mediaapp.service.RatingService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
